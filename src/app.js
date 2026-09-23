32a31835266bd8618c55df8871c9885a6b768ead
Gaiusonyedikachi <gaiusonyedika86@gmail.com>
2026-09-23 10:33:24 -0700

    feat: add /health endpoint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+
 app.get('/', function(req, res) {
   res.json({ message: 'Hello from my own API' });
+});
+
+app.get('/health', function(req, res) {
+  res.json({
+    ststus: 'ok',
+    branch: 'staging',
+    timestamp: new Date().toISOString()
+});
 });
 export default app;
